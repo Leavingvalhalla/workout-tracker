f3e4dd9a59d8fc41a3e90ef00f7e064cc96b5549
Zach Lefler <zachary.lefler@gmail.com>
2023-01-22 14:46:57 -0800

    add outlines for NewRoutine and associated components

---
+++ client/src/components/NewRoutine.js
@@ -0,0 +1,18 @@
+import NewLiftForm from './NewLiftForm';
+import NewRoutineLiftForm from './NewRoutineLiftForm';
+
+function NewRoutine() {
+  return;
+}
+
+export default NewRoutine;
+
+// outline:
+// text: need to add a lift?
+// import: lift form
+
+// input: name of routine
+// button: add new lift
+// import: new routineLift
+
+// TODO:
---
+++ client/src/components/NewRoutineLiftForm.js
@@ -0,0 +1,20 @@
+function NewRoutineLiftForm() {
+  return;
+}
+export default NewRoutineLiftForm;
+
+// outline:
+
+// input: name of lift
+// text: explanation of index
+// input: index - call it day?
+// text: explanation of percentages
+// input: percentage of max
+// text: explain each set gets own lift form
+// input: reps
+// checkbox: amrap
+
+// notes:
+
+// could eventually drag and drop for order, rather than use index
+// could have a hover for explanation rather than always have descriptions
