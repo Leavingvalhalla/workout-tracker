d3c5b25ffb93242feb90494974300e16226d5ae1
Zach Lefler <zachary.lefler@gmail.com>
2023-02-06 18:24:57 -0800

    refactor FinishedWorkout for ts

--- client/src/components/FinishedWorkout.tsx
+++ client/src/components/FinishedWorkout.tsx
@@ -2,28 +2,29 @@ import React from 'react'
 import { Typography } from '@mui/material';
 import { MyConsumer } from './MyContext';
 import { Card, CardContent } from '@mui/material';
+import lift from '../types/lift';
 
 function FinishedWorkout() {
   return (
     <MyConsumer>
-      {(context) => (
+      {(context: any) => (
         <div>
           <Typography sx={{ textAlign: 'center', margin: '5%' }} variant="h1">
             You did it!
           </Typography>
           {context.deloads.length > 0 && (
-            <Card variant="contained" sx={{ maxWidth: '50%', margin: '2%' }}>
+            <Card variant="outlined" sx={{ maxWidth: '50%', margin: '2%' }}>
               <CardContent>
                 <Typography variant="h4">
                   You didn't quite hit your weight on{' '}
                   {context.user.deloads.length === 1 ? 'a lift' : 'some lifts'}{' '}
                   today:
                 </Typography>
-                {context.deloads.map((liftId) => (
+                {context.deloads.map((liftId: string) => (
                   <Typography variant="h5" key={liftId}>
                     Your max weight for{' '}
                     {context.lifts
-                      .filter((lift) => lift.id === liftId)[0]
+                      .filter((lift: lift) => lift.id === liftId)[0]
                       .name.toLowerCase()}{' '}
                     has gone down 10%.
                   </Typography>
@@ -32,14 +33,14 @@ function FinishedWorkout() {
             </Card>
           )}
           {context.increases.length > 0 && (
-            <Card variant="contained" sx={{ maxWidth: '50%', margin: '2%' }}>
+            <Card variant="outlined" sx={{ maxWidth: '50%', margin: '2%' }}>
               <CardContent>
                 <Typography variant="h4">You're moving up!</Typography>
-                {context.increases.map((liftId) => (
+                {context.increases.map((liftId: string) => (
                   <Typography variant="h5" key={liftId}>
                     Your max for{' '}
                     {context.lifts
-                      .filter((lift) => lift.id === liftId)[0]
+                      .filter((lift: lift) => lift.id === liftId)[0]
                       .name.toLowerCase()}{' '}
                     is going up!
                   </Typography>
